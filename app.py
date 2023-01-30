58a90cf4a5e9d04ca91e0e58d1fbc1eca56f2413
eddy106 <119332233+eduardmeister@users.noreply.github.com>
2023-01-30 22:25:31 +0100

    v1

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ db = SQLAlchemy(app)
 
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primery_key=True)
     first_name = db.Column(db.String(100))
     last_name = db.Column(db.String(100))
@@ -17,10 +18,34 @@ class User(db.Model):
     role = db.Column(db.String(10))
     phone = db.Column(db.String(100))
 
+    def to_dict(self):
+        return {col.name: getattr(self, col.name) for col in self.__table__.columns}
+
 
 class Order(db.Model):
+    __tablename__ = 'order'
     id = db.Column(db.Integer, primery_key=True)
+    name = db.Column(db.String(100))
+    description = db.Column(db.String())
+    start_date = db.Column(db.Date)
+    end_date = db.Column(db.Date)
+    address = db.Column(db.String(100))
+    price = db.Column(db.Integer)
+    customer_id = db.Column(db.Integer, db.ForeibnKey('user.id'))
+    executor_id = db.Column(db.Integer, db.ForeibnKey('user.id'))
 
+    def to_dict(self):
+        return {col.name: getattr(self, col.name) for col in self.__table__.columns}
+
+
+class Offer(db.Model):
+    __tablename__ = 'offer'
+    id = db.Column(db.Integer, primery_key=True)
+    order_id = db.Column(db.Integer, db.ForeibnKey('order.id'))
+    executor_id = db.Column(db.Integer, db.ForeibnKey('user.id'))
+
+    def to_dict(self):
+        return {col.name: getattr(self, col.name) for col in self.__table__.columns}
 
 
 if __name__ == '__main__':
