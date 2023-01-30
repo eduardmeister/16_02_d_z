def6dadb7a03cde86861f3d622a29265ae7744cd
eddy106 <119332233+eduardmeister@users.noreply.github.com>
2023-01-30 21:48:59 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primery_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    age = db.Column(db.Integer)
+    email = db.Column(db.String(100))
+    role = db.Column(db.String(10))
+    phone = db.Column(db.String(100))
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primery_key=True)
+
+
+
+if __name__ == '__main__':
+    app.run()
