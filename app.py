cc4759a58b53239d5d7466d80a8626af1afb0941
eddy106 <119332233+eduardmeister@users.noreply.github.com>
2023-02-05 22:21:06 +0100

    v3

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+
 from flask import Flask
 
 from flask_sqlalchemy import SQLAlchemy
+
+import raw_data
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
@@ -10,7 +14,7 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primery_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100))
     last_name = db.Column(db.String(100))
     age = db.Column(db.Integer)
@@ -24,15 +28,15 @@ class User(db.Model):
 
 class Order(db.Model):
     __tablename__ = 'order'
-    id = db.Column(db.Integer, primery_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     description = db.Column(db.String())
     start_date = db.Column(db.Date)
     end_date = db.Column(db.Date)
     address = db.Column(db.String(100))
     price = db.Column(db.Integer)
-    customer_id = db.Column(db.Integer, db.ForeibnKey('user.id'))
-    executor_id = db.Column(db.Integer, db.ForeibnKey('user.id'))
+    customer_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def to_dict(self):
         return {col.name: getattr(self, col.name) for col in self.__table__.columns}
@@ -40,12 +44,28 @@ class Order(db.Model):
 
 class Offer(db.Model):
     __tablename__ = 'offer'
-    id = db.Column(db.Integer, primery_key=True)
-    order_id = db.Column(db.Integer, db.ForeibnKey('order.id'))
-    executor_id = db.Column(db.Integer, db.ForeibnKey('user.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def to_dict(self):
         return {col.name: getattr(self, col.name) for col in self.__table__.columns}
+
+
+with app.app_context():
+    db.create_all()
+
+    for usr_data in raw_data.users:
+        db.session.add(User(**usr_data))
+        db.session.commit()
+
+#im nachfolgendem Text ist ein Fehler in der Zeitformatierung
+
+    #for ord_data in raw_data.orders:
+        #ord_data['start_date'] = datetime.strptime(ord_data['start_date'], '%m/%d/Y').date()
+        #ord_data['end_date'] = datetime.strptime(ord_data['end_date'], '%m/%d/Y').date()
+        #db.session.add(Order(**ord_data))
+        #db.session.commit()
 
 
 if __name__ == '__main__':
