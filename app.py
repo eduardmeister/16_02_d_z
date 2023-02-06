b4a0a14e9b65349705599ae2123adc36302e1f25
eddy106 <119332233+eduardmeister@users.noreply.github.com>
2023-02-06 12:40:43 +0100

    v4

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
-from flask import Flask
+import json
+
+from flask import Flask, request
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,6 +12,10 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
+def get_response(data) -> json:
+    return json.dumps(data), 200, {'Content-Type': 'application/json; charset=utf-8'}
 
 
 class User(db.Model):
@@ -60,10 +66,47 @@ with app.app_context():
         db.session.commit()
 
     for ord_data in raw_data.orders:
-        ord_data['start_date'] = datetime.strptime(ord_data['start_date'], '%m/%d/Y').date()
-        ord_data['end_date'] = datetime.strptime(ord_data['end_date'], '%m/%d/Y').date()
+        ord_data['start_date'] = datetime.strptime(ord_data['start_date'], '%m/%d/%Y').date()
+        ord_data['end_date'] = datetime.strptime(ord_data['end_date'], '%m/%d/%Y').date()
         db.session.add(Order(**ord_data))
         db.session.commit()
+
+    for ofr_data in raw_data.offers:
+        db.session.add(Offer(**ofr_data))
+        db.session.commit()
+
+
+@app.route('/users', methods=['GET', 'POST'])
+def users():
+    if request.method == 'GET':
+        users = User.query.all()
+        res = [usr.to_dict() for usr in users]
+        return get_response(res)
+    elif request.method == 'POST':
+        user_data = json.loads(request.data)
+        db.session.add(User(**user_data))
+        db.session.commit()
+        return '', 201
+
+
+@app.route('/users/<int:uid>', methods=['GET', 'PUT', 'DELITE'])
+def user(uid: int):
+    if request.method == 'GET':
+        user = User.query.get(uid).to_dict()
+        return get_response(user)
+    if request.method == 'DELITE':
+        user = User.query.get(uid)
+        db.session.delite(user)
+        db.session.commit()
+    if request.method == 'PUT':
+        user_data = json.loads(request.data)
+        user = User.query.get(uid)
+        user.first_name = user_data['first_name']
+        user.last_name = user_data['last_name']
+        user.role = user_data['role']
+        user.phone = user_data['phone']
+        user.email = user_data['email']
+        user.age = user_data['age']
 
 
 if __name__ == '__main__':
