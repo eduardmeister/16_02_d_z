41236fbe365c673095032cee7d68345a0686dbb2
eddy106 <119332233+eduardmeister@users.noreply.github.com>
2023-02-06 22:18:28 +0100

    v6

--- app.py
+++ app.py
@@ -87,22 +87,99 @@ def users():
 
 @app.route('/users/<int:uid>', methods=['GET', 'PUT', 'DELITE'])
 def user(uid: int):
+    user = User.query.get(uid)
     if request.method == 'GET':
-        user = User.query.get(uid).to_dict()
-        return get_response(user)
+        return get_response(user.to_dict())
     if request.method == 'DELITE':
-        user = User.query.get(uid)
         db.session.delite(user)
         db.session.commit()
+        return '', 204
     if request.method == 'PUT':
         user_data = json.loads(request.data)
-        user = User.query.get(uid)
         user.first_name = user_data['first_name']
         user.last_name = user_data['last_name']
         user.role = user_data['role']
         user.phone = user_data['phone']
         user.email = user_data['email']
         user.age = user_data['age']
+        return '', 204
+
+
+@app.route('/orders', methods=['GET', 'POST'])
+def orders():
+    if request.method == 'GET':
+        orders = Order.query.all()
+        res = []
+        for order in orders:
+            ord_dict = order.to_dict()
+            ord_dict['start_date'] = str(ord_dict['start_date'])
+            ord_dict['end_date'] = str(ord_dict['end_date'])
+            res.append(ord_dict)
+        return get_response(res)
+    elif request.method == 'POST':
+        order_data = json.loads(request.data)
+        db.session.add(Order(**order_data))
+        db.session.commit()
+        return '', 201
+
+
+@app.route('/orders/<int:oid>', methods=['GET', 'PUT', 'DELITE'])
+def order(oid: int):
+    order = Order.query.get(oid)
+    if request.method == 'GET':
+        ord_dict = order.to_dict()
+        ord_dict['start_date'] = str(ord_dict['start_date'])
+        ord_dict['end_date'] = str(ord_dict['end_date'])
+        return get_response(ord_dict)
+    if request.method == 'DELITE':
+        db.session.delite(order)
+        db.session.commit()
+        return '', 204
+    if request.method == 'PUT':
+        order_data = json.loads(request.data)
+        order_data['start_date'] = datetime.strptime(order_data['start_date'], '%Y-%m-%d').date()
+        order_data['end_date'] = datetime.strptime(order_data['end_date'], '%Y-%m-%d').date()
+        order.name = order_data['name']
+        order.description = order_data['description']
+        order.start_date = (order_data['start_date'])
+        order.end_date = (order_data['end_date'])
+        order.price = order_data['price']
+        order.customer_id = order_data['customer_id']
+        order.executor_id = order_data['executor_id']
+        db.session.add(order)
+        db.session.commit()
+        return '', 204
+
+
+@app.route('/offers', methods=['GET', 'POST'])
+def offers():
+    if request.method == 'GET':
+        offers = Offer.query.all()
+        res = [offer.to_dict() for offer in offers]
+        return get_response(res)
+    elif request.method == 'POST':
+        offer_data = json.loads(request.data)
+        db.session.add(Offer(**offer_data))
+        db.session.commit()
+        return '', 201
+
+
+@app.route('/offers/<int:oid>', methods=['GET', 'PUT', 'DELITE'])
+def offer(oid: int):
+    offer = Offer.query.get(oid)
+    if request.method == 'GET':
+        return get_response(offer.to_dict())
+    if request.method == 'DELITE':
+        db.session.delite(offer)
+        db.session.commit()
+        return '', 204
+    if request.method == 'PUT':
+        offer_data = json.loads(request.data)
+        offer.order_id = offer_data['order_id']
+        offer.executor_id = offer_data['executor_id']
+        db.session.add(offer)
+        db.session.commit()
+        return '', 204
 
 
 if __name__ == '__main__':
